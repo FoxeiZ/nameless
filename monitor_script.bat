5c74f0a21b36a531eca4e7e466884eb0ff968b64
FoxeiZ <foxeiz2004@gmail.com>
2022-03-22 22:14:54 +0700

    update

--- monitor_script.bat
+++ monitor_script.bat
@@ -1,8 +1,9 @@
 :: run in vscode terminal only
 :: in console 1
-FOR /L %L IN (0,0,1) DO @(python main.py&echo Restarting...)
+FOR /L %L IN (0) DO @(python main.py&echo Restarting...)
 
 :: in console 2
-FOR /L %L IN (0,0,1) DO @(E:\script\inotifywait.exe cogs\ -e modify
+FOR /L %L IN (0) DO @(E:\script\inotifywait.exe cogs\ -e modify
 wmic process where "commandline like 'python  main.py'" delete
+cls
 timeout 4)
